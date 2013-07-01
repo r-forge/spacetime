668894bc9d2b66220d101f3c6a25cd768f70119b
jinlong (Jinlong Yang) <jinlong@psu.edu>
2013-07-01 01:02:21 +0000

    - updated notes  - added sttdf_computation.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spacetime@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/trajectories/demo/sttdf_computation.R
@@ -0,0 +1,48 @@
+library(spacetime)
+#sel = 1:2
+sel = TRUE
+lst = list()
+i = j = 1
+#dirs = list.files("Data")[sel]
+dirs = list.files("/Users/jinlong/Documents/R/Geolife Trajectories 1.3/Data/")[sel]
+crs = CRS("+proj=longlat +datum=WGS84")
+pb = txtProgressBar(style = 3, max = length(dirs))
+elev = numeric(0)
+for (d in dirs) {
+	dir = paste("/Users/jinlong/Documents/R/Geolife Trajectories 1.3/Data", d, "Trajectory", sep = "/")
+	#print(dir)
+	for (f in list.files(dir, pattern = "*plt", full.names = TRUE)) {
+		tab = read.csv(f, skip = 6, stringsAsFactors=FALSE)
+		tab$time = as.POSIXct(paste(tab[,6],tab[,7]))
+		tab[tab[,4] == -777, 4] = NA # altitude 
+		tab = tab[,-c(3,5,6,7)]
+		names(tab) = c("lat", "long", "elev", "time")
+		if (all(tab$lat > -90 & tab$lat < 90 & tab$long < 360 
+				& tab$long > -180)) {
+			lst[[i]] = STI(SpatialPoints(tab[,2:1], crs), tab$time)
+			elev = c(elev, tab$elev)
+			i = i+1
+		}
+	}
+	setTxtProgressBar(pb, j)
+	j = j+1
+}
+
+object.size(lst)
+#Bug here??
+#stt = STT(lst)
+stt = STT(STbox = NULL, lst)
+rm(lst)
+dim(stt)
+object.size(stt)
+sttdf = STTDF(stt, data.frame(elev = elev))
+object.size(sttdf)
+
+stt@sp
+stt@time
+stt@traj[[2]]@sp
+stt@traj[[2]]@time
+
+dim(sttdf@data)
+sttdf@data[1:20, ]
+
