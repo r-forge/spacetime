3e95e058c013e01f24146047d0c8f68fd1ba69fb
jinlong (Jinlong Yang) <jinlong@psu.edu>
2013-07-03 05:16:28 +0000

    - Update on STTDF_computation.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spacetime@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/trajectories/demo/sttdf_computation.R
+++ pkg/trajectories/demo/sttdf_computation.R
@@ -47,7 +47,7 @@ object.size(sttdf)
 
 
 
-##
+##TO BE DELETED
 count <- 0
 for( a in sttdf@traj){
   count <- count + length(a@sp)
@@ -55,6 +55,23 @@ for( a in sttdf@traj){
 length(sttdf@traj[[1]]@sp)
 
 ##Game started
+library("maptools")
+
+name <- c("Mecca", "Anchorage", "Washington")
+long <- c(39.823333, -149.883333, -77.0166667)
+lat <- c(21.423333, 61.2166667, 38.9)
+x <- cbind(long, lat)
+row.names(x) <- name
+crib <- c(-9.098363, 56.575960)
+r1 <- gzAzimuth(x[2:3,], x[1,])
+r1
+all.equal(r1, crib)
+r2 <- gzAzimuth(x[2:3,], x[1,], type="abdali")
+r2
+all.equal(r2, crib)
+trackAzimuth(x)
+
+
 
 ##Calcualte the distance between consecutive points for each burst (Unit: km)
 all_dist <- lapply(sttdf@traj, function(x) 
