355bbf72cc062b5467728927658c5d74838c0976
jinlong (Jinlong Yang) <jinlong@psu.edu>
2013-07-13 07:58:58 +0000

    - Updated the description in demo/00Index - Added comments in STItoSTTDFdemo.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spacetime@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/trajectories/demo/STItoSTTDFdemo.R
+++ pkg/trajectories/demo/STItoSTTDFdemo.R
@@ -1,10 +1,26 @@
 ##Demo
-##Without id and trip
-sttdf <- STItoSTTDF(list = lst, elev = elev)
-head(sttdf@data)
+##Create a dummy trajectory with 10 points
+lat <- runif(10, 39, 40)
+long <- runif(10, 115, 116)
+elev <- runif(10, 95, 100)
+t1 <- "2008-10-24 02:09:59"
+t2 <- "2008-10-24 02:10:08" 
+t1 <- as.POSIXct(t1, tz = "GMT")
+t2 <- as.POSIXct(t2, tz = "GMT")
+time <- seq(t1, t2, by = 1)
+traj <- data.frame(lat, long, elev, time)
 
-##With id and trip from GeoLife
-sttdf <- STItoSTTDF(list = lst, id = id, trip = trip, elev = elev)
+##Convert the trajectory into a STI object
+traj <- STI(SpatialPoints(traj[,2:1]), traj$time)
+
+##Store the trajectory into a list
+lst <- list()
+lst[[1]] <- traj
+
+##Demo for STItoSTTDF method
+sttdf <- STItoSTTDF(list = lst, elev = elev)
+
+##Check trajectory attributes computated.
 head(sttdf@data)
 
 ##Check the max and min displacement
