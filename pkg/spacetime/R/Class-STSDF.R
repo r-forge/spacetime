53b6e9aa1efecb795f4302a0c1d185c7ad53ddfb
edzer (Edzer Pebesma) <edzer.pebesma@uni-muenster.de>
2013-09-05 09:28:16 +0000

    removed slots = c(...) notation, to keep R 2.14 compatibility.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spacetime@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/spacetime/R/Class-STSDF.R
+++ pkg/spacetime/R/Class-STSDF.R
@@ -1,6 +1,7 @@
 setClass("STS", # space-time partial 
   contains = "ST", 
-  slots = c(index = "matrix"),
+  #slots = c(index = "matrix"),
+  representation(index = "matrix"),
   validity = function(object) {
     stopifnot(ncol(object@index) == 2)
 	ix = unique(object@index[,1]) # space
@@ -13,7 +14,8 @@ setClass("STS", # space-time partial
 
 setClass("STSDF", # space-time partial data frame
   contains = "STS", 
-  slots = c(data = "data.frame"),
+  #slots = c(data = "data.frame"),
+  representation(data = "data.frame"),
   validity = function(object) {
 	stopifnot(nrow(object@index) == nrow(object@data))
     #stopifnot(ncol(object@index) == 2)
--- pkg/spacetime/R/Class-STTDF.R
+++ pkg/spacetime/R/Class-STTDF.R
@@ -1,6 +1,7 @@
 setClass("STT",  # space-time trajectory/ies without data values
   contains = "ST", 
-  slots = c(traj = "list"),
+  #slots = c(traj = "list"),
+  representation(traj = "list"),
   validity = function(object) {
     stopifnot(length(object@traj) > 0)
 	stopifnot(length(object@sp) == 2)
@@ -13,7 +14,8 @@ setClass("STT",  # space-time trajectory/ies without data values
 
 setClass("STTDF",  # space-time trajectory/ies with data values
   contains = "STT", 
-  slots = c(data = "data.frame"),
+  #slots = c(data = "data.frame"),
+  representation(data = "data.frame"),
   validity = function(object) {
 	stopifnot(sum(sapply(object@traj, length)) == nrow(object@data))
     .checkAttrIsUnique(object@sp, object@time, object@data)
